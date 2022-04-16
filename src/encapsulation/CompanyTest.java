c27237b0d04b42ac909b9a055797834798eb82c9
ramiturkmani <ramiturkmani1@gmail.com>
2022-04-16 12:04:02 -0500

    added company classes

---
+++ src/encapsulation/CompanyTest.java
@@ -0,0 +1,21 @@
+package encapsulation;
+
+public class CompanyTest {
+    public static void main(String[] args) {
+        Company company1 = new Company();
+        Company company2 = new Company();
+
+        company1.setName("Apple");
+        company1.setAddress("California");
+        company1.setPhone("(123) 312-2345");
+
+        company2.setName("Google");
+        company2.setAddress("California");
+        company2.setPhone("(000) 055-5555");
+
+        System.out.println(company1);
+        System.out.println(company2);
+
+        System.out.println("Company 1 = \"" + company1.getName() + "\" and company 2 is = \"" + company2.getName() + "\"");
+    }
+}
